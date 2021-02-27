c91505a8adbd3daaec330de75d3b88a1c11593fa
Shane Harper <cdtharper@gmail.com>
2021-02-27 10:54:25 -0500

    commit before swapping english and morse characters

--- MorseCode.java
+++ MorseCode.java
@@ -6,37 +6,57 @@ public class MorseCode {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Welcome to the Morse code translator. To start, press 1 to\ntranslate from Morse code to English or 2 to translate from\nEnglish to Morse code then press enter: ");
-        String userChoice = input.nextLine();
+        int userChoice = input.nextInt();
+        input.nextLine();
+        System.out.println(userChoice);
+        
+        String[] theArray = getUserPhrase(userChoice, input);
+
+        // for(int i=0; i < theArray.length; i++){
+        //     System.out.println(theArray[i]);
+        // }
+           
+        translateUserPhrase(theArray, userChoice);
+    }
+
+    public static String[] getUserPhrase(int choiceNum, Scanner scan) {
+        int userInputNum = choiceNum;
         String splitString = "";
 
-        System.out.println(userChoice);
-
-        System.out.println();
-        if (userChoice.equals("1")) {
+        if (userInputNum == 1) {
             System.out.print("Please type the English phrase you want to translate.\nPlease only use alphanumeric characters (a-z, 0-9): ");
-        } else if (userChoice.equals("2")) {
+        } else if (userInputNum == 2) {
             System.out.print("Please type the Morse code phrase you want to translate.\nSeparate each letter with a space and each word with a | character.\nExample: '.... . .-.. .-.. --- | .-- --- .-. .-.. -..' (means 'hello world'): ");
             splitString = " ";
         }
-        String userInput = input.nextLine();
-        System.out.println();
-        System.out.println(userInput);
+
+        String userInput = scan.nextLine();
+
         String[] userInputArr = userInput.split(splitString);
 
-        for(int i=0; i < userInputArr.length; i++){
-            System.out.println(userInputArr[i]);
-        }
-
-        // String str = ".... . .-.. .-.. --- | .-- --- .-. .-.. -..";
-        // String splitLetters = " ";
-  
-        // String strArray[] = str.split(splitLetters);
-        
-        
+        return userInputArr;
     }
 
-    // public static Array getArray(strInput) {
-    //     String userInput = strInput;
-    //     return userInput;
-    // }
+    public static void translateUserPhrase(String[] arrInput, int choiceNum) {
+        String[][] englishMorse = {{".-", "A"}, {"B", "-..."}, {"C", "-.-."}, {"D", "-.."}, {"E", "."}, 
+                                    {"F", "..-."}, {"G", "--."}, {"H", "...."}, {"I", ".."}, {"J", ".---"},
+                                    {"K", "-.-"}, {"L", ".-.."}, {"M", "--"}, {"N", "-."}, {"O", "---"},
+                                    {"P", ".--."}, {"Q", "--.-"}, {"R", ".-."}, {"S", "..."}, {"T", "-"},
+                                    {"U", "..-"}, {"V", "...-"}, {"W", ".--"}, {"X", "-..-"}, {"Y", "-.--"},
+                                    {"Z", "--.."}, {"1", ".----"}, {"2", "..---"}, {"3", "...--"}, {"4", "....-"},
+                                    {"5", "....."}, {"6", "-...."}, {"7", "--..."}, {"8", "---.."}, {"9", "----."},
+                                    {"0", "-----"}};
+        
+        String[] arrTranslated = new String[arrInput.length];
+        
+        for (int i = 0; i < arrInput.length; i++) {
+            arrTranslated[i] = englishMorse[i][choiceNum - 1];
+        }
+
+        // String response = arrTranslated.join("");
+
+        for (int j = 0; j < arrTranslated.length; j++) {
+            System.out.println(arrTranslated[j]);
+        }
+    }
 }
